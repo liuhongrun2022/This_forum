f3a1df0ed6297b81fbafec458f70d6a63862de86
Bruce <1978563501@qq.com>
2023-07-13 11:30:35 +0800

    完成登录/注册功能

--- main.py
+++ main.py
@@ -87,11 +87,18 @@ def remove_user(username, password):
 def get_userlist():
     """
     获取用户列表（data/users.fishc）
-    :return: 用户列表
+    :return: 用户列表 [User(username, password), ...]
     """
     with open(PATH_USERS, "rb") as f:
         data = load(f)
     return data
+
+def get_user_infolist():
+    """
+    获取所有用户的 info
+    :return: info [(username, password), ...]
+    """
+    return [user.info() for user in get_userlist()]
 
 def write_into_userlist(data):
     """
@@ -102,18 +109,64 @@ def write_into_userlist(data):
     with open(PATH_USERS, "wb") as f:
         dump(data, f)
 
-padx = 30
-pady = 10
+def login(username, password):
+    """
+    登录进一个账号（如果找不到用户，元组第二项会返回 `None`）
+    :param username: 用户名
+    :param password: 密码
+    :return: (状态码, 找到的用户)
+    """
+    users = get_user_infolist()
+    if (username, password) in users:
+        print("成功登录！")
+
+def register(username, password):
+    """
+    注册一个用户
+    :param username: 用户名
+    :param password: 密码
+    :return: (状态码, 新用户)
+    """
+    # 检查用户名是否已被使用
+    users = get_user_infolist()
+    if username in users:
+        print("用户名已被使用")
+        return
+    # 创建用户
+    return add_user(username, password)
+
+def command_login():
+    """
+    作为 button 的 `command` 属性的函数，用于调用 `login()`
+    :return: None
+    """
+    value_username = entry_username.get()
+    value_password = entry_password.get()
+    login(value_username, value_password)
+
+def command_register():
+    """
+    作为 button 的 `command` 属性的函数，用于调用 `register()`
+    :return: None
+    """
+    value_username = entry_username.get()
+    value_password = entry_password.get()
+    register(value_username, value_password)
+
+option = {"padx": 30, "pady": 10}
 root = Window("This Forum 1.0 Beta", "morph")
 root.geometry("1800x1000+50+50")
 label_username = Label(root, bootstyle="dark", text="用户名")
-label_username.grid(row=0, column=0, padx=padx, pady=pady)
+label_username.grid(row=0, column=0, **option, columnspan=2)
 entry_username = Entry(root, bootstyle="info", width=30)
-entry_username.grid(row=1, column=0, padx=padx, pady=pady)
+entry_username.grid(row=1, column=0, **option, columnspan=2)
 label_password = Label(root, bootstyle="dark", text="密码")
-label_password.grid(row=2, column=0, padx=padx, pady=pady)
+label_password.grid(row=2, column=0, **option, columnspan=2)
 entry_password = Entry(root, bootstyle="primary", width=30, show="*")
-entry_password.grid(row=3, column=0, padx=padx, pady=pady)
-button_login = Button(root, bootstyle="success", text="登录")
-button_login.grid(row=4, column=0, padx=padx, pady=pady, sticky=W)
+entry_password.grid(row=3, column=0, **option, columnspan=2)
+button_login = Button(root, bootstyle="success", text="登录", width=8)
+button_login["command"] = command_login
+button_login.grid(row=4, column=0, **option)
+button_register = Button(root, bootstyle="info", text="注册", width=8)
+button_register.grid(row=4, column=1, **option)
 root.mainloop()
